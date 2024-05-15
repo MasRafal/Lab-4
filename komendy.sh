0634f149acd6e27eb5d5befd68d7a3b270dd43e6
Rafał Masternak <MasRafal>
2024-05-15 15:01:47 +0200

    lab-3

---
+++ komendy.sh
@@ -0,0 +1,64 @@
+git commit
+git commit
+
+git checkout -b bugFix
+
+git checkout -b bugFix
+git commit
+git checkout main
+git commit
+git merge bugFix
+
+git checkout -b bugFix
+git commit
+git checkout main
+git commit
+git rebase main
+
+git checkout C4
+
+git checout bugFix^
+
+git branch -f main C6
+git branch -f bugFix C0
+git checkout HEAD^ 
+
+git reset HEAD^
+git checkout pushed
+git revert HEAD
+
+git cherry-pick C3 C4 C7 
+
+git rebase -i HEAD~4 
+
+git rebase -i HEAD~3
+git branch -f main C4' 
+
+git rebase -i HEAD~2
+git commit --amend
+git rebase -i HEAD~2
+git branch -f main C3'' 
+
+git checkout main
+git cherry-pick C2
+git commit --amend
+git cherry-pick C3
+
+git tag v0 C1 
+git tag v1 C2 
+git checkout C2 
+
+git commit
+
+git rebase master bugFix
+git rebase bugFix side
+git rebase side another
+git rebase another master
+
+git branch bugWork master~^2~
+
+git checkout one
+git cherry-pick C4 C3 C2
+git checkout two
+git cherry-pick C5 C4 C3 C2
+git branch -f three C2
